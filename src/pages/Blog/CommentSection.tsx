5d7e43a4382a05ac901ee41d3ea0de56a30fe9fd
fwx5618177 <fengwenxuan2006@126.com>
2025-03-28 23:28:49 +0800

    chore: update

--- src/pages/Blog/CommentSection.tsx
+++ src/pages/Blog/CommentSection.tsx
@@ -3,6 +3,12 @@ import { Comment } from './types';
 import CommentItem from './CommentItem';
 import styles from './comment-section.module.scss';
 import classnames from 'classnames';
+import UsersIcon from './icons/UsersIcon';
+import GoogleIcon from './icons/GoogleIcon';
+import FacebookIcon from './icons/FacebookIcon';
+import TwitterIcon from './icons/TwitterIcon';
+import GitHubIcon from './icons/GitHubIcon';
+import UserIcon from './icons/UserIcon';
 
 const CommentSection: React.FC<{ comments: Comment[] }> = ({ comments }) => {
   const [newComment, setNewComment] = useState('');
@@ -82,20 +88,7 @@ const CommentSection: React.FC<{ comments: Comment[] }> = ({ comments }) => {
         {showLoginOptions && (
           <div className={styles.commentLoginSection}>
             <h4 className={styles.loginHeading}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
-                <circle cx="9" cy="7" r="4"></circle>
-                <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
-                <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
-              </svg>
+              <UsersIcon />
               Sign in to comment
             </h4>
             <p className={styles.loginText}>
@@ -108,36 +101,28 @@ const CommentSection: React.FC<{ comments: Comment[] }> = ({ comments }) => {
                 className={classnames(styles.socialLoginButton, styles.google)}
                 onClick={() => handleLogin('google')}
               >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                  <path d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z" />
-                </svg>
+                <GoogleIcon />
                 Google
               </button>
               <button
                 className={classnames(styles.socialLoginButton, styles.facebook)}
                 onClick={() => handleLogin('facebook')}
               >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                  <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
-                </svg>
+                <FacebookIcon />
                 Facebook
               </button>
               <button
                 className={classnames(styles.socialLoginButton, styles.twitter)}
                 onClick={() => handleLogin('twitter')}
               >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                  <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
-                </svg>
+                <TwitterIcon />
                 Twitter
               </button>
               <button
                 className={classnames(styles.socialLoginButton, styles.github)}
                 onClick={() => handleLogin('github')}
               >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                  <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
-                </svg>
+                <GitHubIcon />
                 GitHub
               </button>
             </div>
@@ -163,66 +148,31 @@ const CommentSection: React.FC<{ comments: Comment[] }> = ({ comments }) => {
                 <div className={styles.userPlatform}>
                   {currentUser.platform === 'google' && (
                     <>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="currentColor"
-                      >
-                        <path d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z" />
-                      </svg>
+                      <GoogleIcon />
                       Signed in with Google
                     </>
                   )}
                   {currentUser.platform === 'facebook' && (
                     <>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="currentColor"
-                      >
-                        <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
-                      </svg>
+                      <FacebookIcon />
                       Signed in with Facebook
                     </>
                   )}
                   {currentUser.platform === 'twitter' && (
                     <>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="currentColor"
-                      >
-                        <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
-                      </svg>
+                      <TwitterIcon />
                       Signed in with Twitter
                     </>
                   )}
                   {currentUser.platform === 'github' && (
                     <>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="currentColor"
-                      >
-                        <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
-                      </svg>
+                      <GitHubIcon />
                       Signed in with GitHub
                     </>
                   )}
                   {currentUser.platform === 'guest' && (
                     <>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      >
-                        <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-                        <circle cx="12" cy="7" r="4"></circle>
-                      </svg>
+                      <UserIcon />
                       Commenting as Guest
                     </>
                   )}
@@ -255,7 +205,7 @@ const CommentSection: React.FC<{ comments: Comment[] }> = ({ comments }) => {
 
             <textarea
               className={styles.commentTextarea}
-              placeholder="Share your thoughts..."
+              placeholder="Write your comment here..."
               value={newComment}
               onChange={e => setNewComment(e.target.value)}
               required
@@ -268,7 +218,15 @@ const CommentSection: React.FC<{ comments: Comment[] }> = ({ comments }) => {
 
             <div className={styles.commentFooter}>
               <p className={styles.commentPolicy}>
-                By submitting this comment, you agree to our <a href="#">community guidelines</a>.
+                By submitting a comment, you agree to our{' '}
+                <a href="/privacy-policy" target="_blank" rel="noopener noreferrer">
+                  Privacy Policy
+                </a>{' '}
+                and{' '}
+                <a href="/terms-of-service" target="_blank" rel="noopener noreferrer">
+                  Terms of Service
+                </a>
+                .
               </p>
               <button type="submit" className={styles.commentSubmit}>
                 Post Comment
--- src/pages/Blog/PostNavigation.tsx
+++ src/pages/Blog/PostNavigation.tsx
@@ -3,6 +3,7 @@ import classnames from 'classnames';
 import { Link } from 'react-router-dom';
 import { PostDetail } from './types';
 import { formatDate } from '@/utils/formatDate';
+import StarIcon from './icons/StarIcon';
 
 const PostNavigation: React.FC<{ previous: PostDetail | null; next: PostDetail | null }> = ({
   previous,
@@ -12,17 +13,7 @@ const PostNavigation: React.FC<{ previous: PostDetail | null; next: PostDetail |
     <div className={styles.postNavigation}>
       <div className={styles.container}>
         <h3 className={styles.postNavHeading}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
-          </svg>
+          <StarIcon />
           Continue Your Reading Journey
         </h3>
         <div className={styles.postNavLinks}>
